d42a6a70640c12a44b086d1ad84ec54335720d44
11121997 <nawalkhaled27@yahoo.com>
2023-12-15 07:33:06 -0800

    update

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -54,6 +54,8 @@ class User():
             return False
         if self.__password is None:
             return False
+        if pwd == self.__password:
+            return True
         return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
